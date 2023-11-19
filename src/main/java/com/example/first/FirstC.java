3c17a53fe52681b92a03c99b6190988427fa3ba0
Alexey <alexlatkin3@gmail.com>
2023-11-19 22:28:53 +0300

    First commit

---
+++ src/main/java/com/example/first/FirstC.java
@@ -0,0 +1,12 @@
+package com.example.first;
+
+public class FirstC {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+
+        String newString = "HI";
+
+
+    }
+}
